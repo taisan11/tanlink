5ca503706edbbafad8f390527b9f7ab011c3f285
taisan <131413638+taisan11@users.noreply.github.com>
2023-11-23 18:11:55 +0900

    a

--- server.ts
+++ server.ts
@@ -1,15 +1,30 @@
+// deno-lint-ignore-file
 /// <reference lib="deno.unstable" />
 import { Hono } from "https://deno.land/x/hono@v3.10.2/mod.ts";
 import { customAlphabet } from "npm:nanoid@3.1.16";
 
 const app = new Hono();
-// const kv = await Deno.openKv("kv.sqlite");
+const kv = await Deno.openKv();
 
 const nanoid = customAlphabet(
   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
   7,
 );
 
+async function shorten(url: string) {
+  const key = nanoid();
+  await kv.set([key], url);
+  return { key };
+}
+
 app.get("/", (c) => c.text(nanoid()));
+app.get("/tan/:url", async (c) => {
+  const url = c.req.param("url");
+  const { key } = await shorten(url);
+  return c.text(key);
+});
+app.get("/:id", async (c) => {
+  const id = c.req.param("id");
+});
 
 Deno.serve(app.fetch);
