675977f476d6b45e00dc5f427d0e4acc27e9d234
taisan <131413638+taisan11@users.noreply.github.com>
2023-11-25 08:28:31 +0900

    a

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@
 /// <reference lib="deno.unstable" />
 import { Hono } from "https://deno.land/x/hono@v3.10.2/mod.ts";
 import { customAlphabet } from "npm:nanoid@3.1.16";
-import { logger } from 'https://deno.land/x/hono/middleware.ts'
+import { logger } from "https://deno.land/x/hono@v3.10.2/middleware.ts"
 
 const app = new Hono();
 const kv = await Deno.openKv();
