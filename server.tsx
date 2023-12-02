d27f4ffaa52fb8f75582acf3207bea2db50d2fa8
taisan <131413638+taisan11@users.noreply.github.com>
2023-12-02 17:30:01 +0900

    haa~

---
+++ server.tsx
@@ -0,0 +1,70 @@
+// deno-lint-ignore-file
+/** @jsx jsx */
+/** @jsxFrag Fragment */
+/// <reference lib="deno.unstable" />
+import { Hono } from "https://deno.land/x/hono@v3.10.2/mod.ts";
+import { customAlphabet } from "npm:nanoid@3.1.16";
+import { logger,compress,html,jsx } from "https://deno.land/x/hono@v3.10.2/middleware.ts"
+
+const app = new Hono();
+const kv = await Deno.openKv();
+
+const nanoid = customAlphabet(
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+  7,
+);
+
+async function shorten(url: string) {
+  const key = nanoid();
+  await kv.set([key], url);
+  return { key };
+}
+//urlcheck
+function urlcheck(string: string) {
+  try {
+    new URL(string); return true; 
+  } catch (err) { 
+    return false; 
+  }
+}
+
+app.use("*", compress({ encoding: "gzip" }));
+app.use('*', logger())
+const Layout = (props: Props) =>
+  html`<!DOCTYPE html><html><head><title>${props.title}</title><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>${props.children}</body></html>`;
+app.get("/", async (c) => {
+  const url: string = c.req.query('url');
+  if (!url) return c.html(
+    <Layout title="たんLink">
+      <h1>たんLink</h1>
+      <p>URLを短縮できるサービスです</p>
+      <p>https://tanlink.deno.dev/?url=短縮したいURL</p>
+      <p>https://tanlink.deno.dev/短縮後のキー</p>
+    </Layout>,
+  );
+  if (!urlcheck(url)) return c.text("URLじゃないよ");
+  const { key } = await shorten(url);
+  return c.html(
+    <Layout title="たんLink">
+      <h1>たんLink</h1>
+      <p>短縮URL</p>
+      <a href={`https://tanlink.deno.dev/${key}`}>{`https://tanlink.deno.dev/${key}`}</a>
+      <button onClick={copyToClipboard}>Copy URL</button>
+      <script>
+        const copyToClipboard = () => {
+        const copyTarget = document.getElementById("copyTarget");
+        copyTarget.select();
+        document.execCommand("Copy");
+        };
+      </script>
+    </Layout>
+  );
+});
+app.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  const aredayo = await kv.get([id])
+  return c.redirect(aredayo.value);
+  // return c.text(aredayo.value);
+});
+
+Deno.serve(app.fetch);
