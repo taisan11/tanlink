7646dac7e00a60b48653ad33ba82bdf200f75619
taisan <131413638+taisan11@users.noreply.github.com>
2023-12-23 14:05:10 +0900

    m

--- server.tsx
+++ server.tsx
@@ -4,7 +4,13 @@
 /// <reference lib="deno.unstable" />
 import { Hono } from "https://deno.land/x/hono@v3.10.2/mod.ts";
 import { customAlphabet } from "npm:nanoid@3.1.16";
-import { logger,compress,html,jsx,serveStatic } from "https://deno.land/x/hono@v3.10.2/middleware.ts"
+import {
+  compress,
+  html,
+  jsx,
+  logger,
+  serveStatic,
+} from "https://deno.land/x/hono@v3.10.2/middleware.ts";
 
 const app = new Hono();
 const kv = await Deno.openKv();
@@ -22,47 +28,52 @@ async function shorten(url: string) {
 //urlcheck
 function urlcheck(string: string) {
   try {
-    new URL(string); return true; 
-  } catch (err) { 
-    return false; 
+    new URL(string);
+    return true;
+  } catch (err) {
+    return false;
   }
 }
 
 app.use("*", compress({ encoding: "gzip" }));
-app.use('*', logger())
+app.use("*", logger());
 const Layout = (props: Props) =>
   html`<!DOCTYPE html><html><head><title>${props.title}</title><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>${props.children}</body></html>`;
 app.get("/", async (c) => {
-  const url: string = c.req.query('url');
-  if (!url) return c.html(
-    <Layout title="たんLink">
-      <h1>たんLink</h1>
-      <p>URLを短縮できるサービスです</p>
-      <p>https://tanlink.deno.dev/?url=短縮したいURL</p>
-      <p>https://tanlink.deno.dev/短縮後のキー</p>
-    </Layout>,
-  );
+  const url: string = c.req.query("url");
+  if (!url) {
+    return c.html(
+      <Layout title="たんLink">
+        <h1>たんLink</h1>
+        <p>URLを短縮できるサービスです</p>
+        <p>https://tanlink.deno.dev/?url=短縮したいURL</p>
+        <p>https://tanlink.deno.dev/短縮後のキー</p>
+      </Layout>,
+    );
+  }
   if (!urlcheck(url)) return c.text("URLじゃないよ");
   const { key } = await shorten(url);
   return c.html(
     <Layout title="たんLink">
       <h1>たんLink</h1>
       <p>短縮URL</p>
-      <a href={`https://tanlink.deno.dev/${key}`} id="a">{`https://tanlink.deno.dev/${key}`}</a>
+      <a href={`https://tanlink.deno.dev/${key}`} id="a">
+        {`https://tanlink.deno.dev/${key}`}
+      </a>
       <button onclick="copyToClipboard()">Copy text</button>
       <script src="./onclick.js"></script>
-    </Layout>
+    </Layout>,
   );
 });
 app.get("/:id", async (c) => {
   const id = c.req.param("id");
-  const aredayo = await kv.get([id])
+  const aredayo = await kv.get([id]);
   return c.redirect(aredayo.value);
   // return c.text(aredayo.value);
 });
 app.get(
   "/onclick.js",
-  serveStatic("./onclick.js")
+  serveStatic("./onclick.js"),
 );
 
 Deno.serve(app.fetch);
