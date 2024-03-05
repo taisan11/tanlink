722b7843244bb1761aad74889f4298f20c1ba066
taisan <131413638+taisan11@users.noreply.github.com>
2024-03-05 14:01:40 +0900

    応急

--- server.tsx
+++ server.tsx
@@ -10,7 +10,9 @@ import {
   logger,
   serveStatic,
   secureHeaders ,
-} from "https://deno.land/x/hono@v3.10.2/middleware.ts";
+  basicAuth ,
+  jsxRenderer
+} from "https://deno.land/x/hono@v4.0.7/middleware.ts";
 
 const app = new Hono();
 const kv = await Deno.openKv();
@@ -19,6 +21,14 @@ const nanoid = customAlphabet(
   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
   7,
 );
+
+app.use(
+  '/auth/*',
+  basicAuth({
+    username: 'taijn',
+    password: 'njl',
+  })
+)
 
 async function shorten(url: string) {
   const key = nanoid();
@@ -38,60 +48,65 @@ function urlcheck(string: string) {
 app.use('*', secureHeaders())
 app.use("*", compress({ encoding: "gzip" }));
 app.use("*", logger());
-app.use(async (c, next) => {
-  c.setRenderer((content) => {
-    return c.html(
+app.get(
+  '*',
+  jsxRenderer(({ children }) => {
+    return (
       <html>
         <head>
           <title>たんりんく</title>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta charset="UTF-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </head>
-        <body>{content}</body>
+        <body>{children}</body>
       </html>
     )
   })
-  await next()
-})
+)
 app.get("/", async (c) => {
   const url: string = c.req.query("url");
   if (!url) {
-    return c.html(
-      <Layout title="たんLink">
+    return c.render(
+      <div>
         <h1>たんLink</h1>
         <p>URLを短縮できるサービスです</p>
         <input type="text" name="a" id="a" />
         <button onclick></button>
-        {html`
-          
-        `}
         <p>https://tanlink.deno.dev/?url=短縮したいURL</p>
         <p>https://tanlink.deno.dev/短縮後のキー</p>
-      </Layout>,
+        <p>https://tanlink.deno.dev/auth/?url=aaaa&key=aaaa</p>
+      </div>
     );
   }
   if (!urlcheck(url)) return c.text("URLじゃないよ");
   const { key } = await shorten(url);
-  return c.html(
-    <Layout title="たんLink">
+  return c.render(
+    <div>
       <h1>たんLink</h1>
       <p>短縮URL</p>
       <a href={`https://tanlink.deno.dev/${key}`} id="a">
         {`https://tanlink.deno.dev/${key}`}
       </a>
-      <button onclick="copyToClipboard()">Copy text</button>
-      <script src="./onclick.js"></script>
-    </Layout>,
+    </div>,
   );
 });
-app.get(
-  "/onclick.js",
-  serveStatic("./onclick.js"),
-);
 app.get("/:id", async (c) => {
   const id = c.req.param("id");
   const aredayo = await kv.get([id]);
   return c.redirect(aredayo.value);
   // return c.text(aredayo.value);
 });
+app.get('/auth',async (c) => {
+  const url: string = c.req.query("url");
+  const key: string = c.req.query("key");
+  await kv.set(key, url);
+  return c.render(
+    <div>
+      <h1>たんLink</h1>
+      <p>短縮URL</p>
+      <a href={`https://tanlink.deno.dev/${key}`} id="a">
+        {`https://tanlink.deno.dev/${key}`}
+      </a>
+    </div>  )
+})
 Deno.serve(app.fetch);
