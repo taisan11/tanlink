5ca414875790228786c3fd58812dcbe3e8b5f8e6
taisan11 <131413638+taisan11@users.noreply.github.com>
2025-02-08 19:37:06 +0900

    a

--- admin.tsx
+++ admin.tsx
@@ -1,8 +1,14 @@
 import { Hono } from "hono"
 import { kv } from "./server.tsx"
 import { jwt,verify,decode,sign } from 'hono/jwt'
+import {getSignedCookie,setSignedCookie} from "hono/cookie"
 import type { JwtVariables } from 'hono/jwt'
 import type {HonoJsonWebKey, JWTPayload} from "hono/utils/jwt/types"
+import {User} from "./kv_types.ts"
+
+interface UserToken extends JWTPayload {
+    UserID: string;
+}
 
 interface Env {
   Variables: {
@@ -15,15 +21,35 @@ const env = Deno.env
 export const jwk = "" 
 
 app.use(async(c,next)=>{
-    if (!c.req.header("Authorization")) {
+    //取得.認証
+    const SECRET_KEY = env.get("SECRET_KEY")!
+    const base_token = await getSignedCookie(c,SECRET_KEY,"token","secure")
+    if (!base_token) {
         return c.redirect("/auth/login")
     }
-    let token: JWTPayload;
+    let token: UserToken;
     try {
-        token = await verify(c.req.header("Authorization")!, env.get("SECRET_KEY")!);
+        token = await verify(base_token, SECRET_KEY) as UserToken;
+        kv.get(["nowtoken",token.UserID]).then((r)=>{
+            if(r.value !== token) return c.redirect("/auth/login")
+        })
     } catch (e) {
         return c.redirect("/auth/login");
     }
+    if (!token.UserID) {
+        return c.redirect("/auth/login");
+    }
+    //更新
+    token.exp = Math.floor(Date.now() / 1000) + 60 * 60;
+    token.iat = Math.floor(Date.now() / 1000);
+    token.nbf = Math.floor(Date.now() / 1000);
+    //保存
+    await setSignedCookie(c,SECRET_KEY,"token",await sign(token,SECRET_KEY),{httpOnly:true,sameSite:"Lax",secure:true})
+    kv.set(["nowtoken",token.UserID],token)
+    kv.get(["Users",token.UserID]).then((r)=>{
+        const user = r.value as User | null;
+        if (!user || !user.admin) return c.redirect("/auth/login");
+    })
     c.set("payload", token);
     await next()
 })
---
+++ kv_types.ts
@@ -0,0 +1,12 @@
+export interface url {
+    url: string;
+}
+
+export interface urlOption {
+    IPaddr: string;
+}
+
+export interface User {
+    UserID: string;
+    admin: boolean;
+}
